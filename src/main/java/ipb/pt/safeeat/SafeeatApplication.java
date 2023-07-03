7eced4950889663570f09906950f7df3dd1a9109
Miguel Beckers <128187386+miguel57403@users.noreply.github.com>
2023-07-03 23:25:47 +0100

    initialized data creation

--- src/main/java/ipb/pt/safeeat/SafeeatApplication.java
+++ src/main/java/ipb/pt/safeeat/SafeeatApplication.java
@@ -1,11 +1,32 @@
 package ipb.pt.safeeat;
 
+import ipb.pt.safeeat.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SafeeatApplication implements CommandLineRunner {
+    @Autowired
+    AddressRepository addressRepository;
+    @Autowired
+    AdvertisementRepository advertisementRepository;
+    @Autowired
+    CartRepository cartRepository;
+    @Autowired
+    DeliveryRepository repository;
+    @Autowired
+    FeedbackRepository feedbackRepository;
+    @Autowired
+    IngredientRepository ingredientRepository;
+    @Autowired
+    ItemRepository itemRepository;
+    @Autowired
+    NotificationRepository notificationRepository;
+    @Autowired
+    OrderRepository
+
     public static void main(String[] args) {
         SpringApplication.run(SafeeatApplication.class, args);
     }
