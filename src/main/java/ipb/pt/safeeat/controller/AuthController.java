3645d0bbb5832b53931bc35eae6be39cbd1a5764
miguel57403 <128187386+miguel57403@users.noreply.github.com>
2023-07-10 20:37:43 +0100

    Update AuthController.java

--- src/main/java/ipb/pt/safeeat/controller/AuthController.java
+++ src/main/java/ipb/pt/safeeat/controller/AuthController.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin
 @Controller
 public class AuthController {
 
