1b84a798a5bf530e00e2f9398ba8c3e2dc73125d
daniel57060 <a57060@alunos.ipb.pt>
2023-07-10 20:46:48 +0100

    Import CrossOrigin

--- src/main/java/ipb/pt/safeeat/controller/AuthController.java
+++ src/main/java/ipb/pt/safeeat/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
